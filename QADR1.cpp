8e9a322634433441676b1c03ee462328a6d6d404
Artoriusain <artoriusale@gmail.com>
2024-08-20 20:03:41 +0300

    много всего, особенно беза кода

--- QADR1.cpp
+++ QADR1.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-int sqrslv(double a, double b, double c, double *x1, double *x2) {
-
 //-------------------------------------------------------------
 //! Solves a square equation using ax^2 + bx + c= 0
 //!
@@ -12,84 +10,86 @@ int sqrslv(double a, double b, double c, double *x1, double *x2) {
 //! @param [out] x1 The 1st (and sometimes only) root (pointer)
 //! @param [out] x2 The 2nd root (pointer)
 //!
-//! @return Number of roots, roots
+//! @return Number of roots
 //!
 //! @note In case of infinite number of roots,
-//! returns INF.
+//! returns INF_ROOTS.
 //-------------------------------------------------------------
 
+enum RootNumber {
+    ZERO_ROOTS = 0,
+    ONE        = 1,
+    TWO        = 2,
+    INF_ROOTS  = 3,
+};
 
+RootNumber square_solve(double a, double b, double c, double *x1, double *x2) {
+    double D = b*b - 4*a*c;
 
+    if (a == 0 && b == 0) {
+        return INF_ROOTS;
+}
+    if (a == 0 && b != 0) {
+        *x1 = -c/b;
+        return ONE;
+}
+    if (D < 0) {
+        return ZERO_ROOTS;
+}
+    double root_D = sqrt(D);
+    *x1 = (-b + root_D)/(2*a);
+    *x2 = (-b - root_D)/(2*a);
 
-
- double D;
- D = b*b - 4*a*c;
-
-
- if (a == 0 && b == 0)  {
-     return 'INF';
-    }
- if (a == 0 && b != 0) {
-     *x1 = -c/b;
-     return 1;
- }
-  if (D < 0) {
-     return 0;
-  }
-
- *x1 = (-b + sqrt(D))/(2*a);
- *x2 = (-b - sqrt(D))/(2*a);
-
- if (*x1 == *x2) {
-     return 1;
- }
-
-    return 2;
+    if (*x1 == *x2) {
+        return ONE;
+}
+    return TWO;
 }
 
 
 int main() {
+    double a = 0, b = 0, c = 0;
+    int coef_count = 0;
 
- double a, b, c;
+    printf("# ПОЖАЛУЙСТА ВВЕДИ КОЭФФИЦИЕНТЫ\n");
 
- printf("#enter factor a\n");
- scanf("%lg", &a);
- printf("#a = %g\n", a);
+    while(1) {
+        coef_count = scanf("%lg %lg %lg", &a, &b, &c);
 
- printf("#enter factor b\n");
- scanf("%lg", &b);
- printf("b = %g\n", b);
+        if (coef_count == 3) {
+            break;
+}
+        printf("НЕПРАВИЛЬНО, ПОПРОБУЙ ЕЩЁ РАЗ\n");
 
- printf("#enter factor c\n");
- scanf("%lg", &c);
- printf("#c = %g\n", c);
+        while ((cur_ch = getchar()) != '\n' && cur_ch != EOF) {}
+}
 
- double x1, x2;
- x1 = x2 = 0;
+    printf("# МЫ СПРАВИЛИСЬ \n a = %lg\n b = %lg\n c = %lg\n", a, b, c);
 
- int var = sqrslv(a, b, c, &x1, &x2);
+    double x1 = 0, x2 = 0;
 
+    RootNumber root_count = square_solve(a, b, c, &x1, &x2);
 
- switch(var) {
+    switch(root_count) {
 
-    case 0:  printf("#СОРИ БРАТАНЧИК КОРНЕЙ НЕТ");
-             break;
+        case 0:         printf("# СОРИ БРАТАНЧИК КОРНЕЙ НЕТ\n");
+                        break;
 
-    case 1:  printf("#ОПАААА КОРЕНЬ ОДИН И ЭТО ");
-             printf("%g", x1);
-             break;
+        case 1:         printf("# ОПАААА КОРЕНЬ ОДИН И ЭТО ");
+                        printf("%lg\n", x1);
+                        break;
 
-    case 2:  printf("#ДВА КОРНЯ РОДНОЙ ");
-             printf("%g or %g", x1, x2);
-             break;
+        case 2:         printf("# ДВА КОРНЯ РОДНОЙ ");
+                        printf("%lg or %lg\n", x1, x2);
+                        break;
 
-    case 'INF':  printf("#INFIMNOGA ROOTS");
-                 break;
+        case 3:         printf("# INFIMNOGA ROOTS\n");
+                        break;
 
-    default: printf("#МЫ НАПОРТАЧИЛИ");
-             break;
+        default:        printf("# МЫ НАПОРТАЧИЛИ\n");
+                        break;
 
- }
+}
 
 
 
